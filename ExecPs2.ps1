89c7ea417605391fbce0e0ce87adbbfabcebac43
Max <mmmaxxx@gmail.com>
2026-04-23 20:10:26 +0300

    Ok2

---
+++ ExecPs2.ps1
@@ -0,0 +1,10 @@
+#$computers = "PC1"
+
+# Проверяем компьютеры
+if (-not $computers) {
+    Write-Warning "Не найдено компьютеров. Должен быть задан список в $computers "
+    Pause
+    exit
+}
+
+$computers
